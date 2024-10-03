acefeaf7864dfa99c899a7014e7aeea27c1d32d7
Oumayma Msaed <oumaymamsd@gmail.com>
2024-10-03 21:24:43 +0100

    fixed errors and added materials

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("This works");
+    }
+}
